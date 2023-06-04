46a9400180092f26d24f7fdf64f7cf8aeadeb242
leejhn11 <39404451+leejhn11@users.noreply.github.com>
2023-06-04 17:55:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-let expression = '';
+let inputString = '';
+let displayValue = '';
+let resultValue = '';
+let operator = '';
 
-function appendInput(value) {
-  expression += value;
-  updateDisplay();
+function input(value) {
+  inputString += value;
+  displayValue += value;
+  document.querySelector('.display').value = displayValue
+  }
+
+function calculate() {
+  let calculation = inputString.split(operator);
+  let operand1 = parseFloat(calculation[0]);
+  let operand2 = parseFloat(calculation[1]);
+  switch(operator) {
+    case '+':
+      resultValue = operand1 + operand2;
+      break;
+    case '-':
+      resultValue = operand1 - operand2;
+      break;
+    case '*':
+      resultValue = operand1 * operand2;
+      break;
+    case '/':
+      resultValue = operand1 / operand2;
+    break;
+    default:
+      resultValue = '';
+  }
+  document.querySelector('.display').value = resultValue;
+  inputString = resultValue.toString();
+  displayValue = '';
 }
 
 function clearDisplay() {
-  expression = '';
-  updateDisplay();
+  inputString = '';
+  displayValue = '';
+  resultValue = '';
+  operator = '';
+  document.querySelector('.display').value = '';
 }
 
-function evaluate() {
-  try {
-    const result = eval(expression);
-    expression = result.toString();
-  } catch (error) {
-    expression = 'Error';
-  }
-  updateDisplay();
-}
-
-function updateDisplay() {
-  const display = document.getElementById('display');
-  display.value = expression;
+function backspace() {
+  inputString = inputString.slice(0, -1);
+  displayValue = displayValue.slice(0, -1);
+  document.querySelector('.display').value = displayValue;
 }
